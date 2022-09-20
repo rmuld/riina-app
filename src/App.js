4189ae8eb1d3bceb411395e6ddce79c49f054dca
LAPTOP-VSSU1O4B\Riina <riinamuld@gmail.com>
2022-09-20 09:38:22 +0300

    create inital thesis title page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const studentList = ["Kati Karu", "Mati Taru"];
+  const instructorList = ["Jaagup Kippar", "Martti Raavel"]
+  const departmentList = ["Haapsalu Kolledž", "Balti Filmi ja Meedia Kool"];
+  const subDepartmentList = ["rakendusinformaatika", "audiovisuaalne meedia"];
+
+  function addStudent() {
+    
+  }
+
+  function removeStudent() {
+      
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App-header">
+      <div className='title-header'>
+        <h1>TALLINNA ÜLIKOOL</h1>
+
+      <div className="dropdown">
+      <button className="dropbtn">Vali allüksus</button>
+        {departmentList.length
+        ? 
+          <div className="dropdown-content">
+            {departmentList.map((item) => {
+              <h2 key={item.department}>{ item.department}</h2>
+            })}
+            </div>
+          : <h2>ALLÜKSUS PUUDUB</h2>}
+      </div>
+      <div className='sub-department'>
+        <p>Rakendusinformaatika</p>
+      </div>
+
+      <div className='thesis-title'>
+            <p>Riina Muld</p>
+        <h2>my thesis title</h2>
+        <p>Diplomitöö</p>
+      </div>
+
+      <div className='thesis-instructor'>
+          <p>Juhendaja: Jaagup Kippar</p> 
+      </div>
+      
+      <div className='thesis-footer'>
+            <p>Haapsalu 2022</p>
+      </div>
+
     </div>
+    // </div>
+
   );
 }
 
